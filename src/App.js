f6b71b74cc9e0004e4e96a6b24ade281a2c1f38a
unknown <sam7073@naver.com>
2020-10-28 10:32:54 +0900

    10/28 basic todo completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
-import React from "react";
-import TodoInsert from "./components/TodoInsert";
-import TodoList from "./components/TodoList";
-import TodoTemplate from "./components/TodoTemplate"
+import React, { useState, useRef, useCallback } from 'react';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+import TodoTemplate from './components/TodoTemplate';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: 'component styling',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: 'make todo app',
+      checked: false,
+    },
+  ]);
+  const nextId = useRef(4);
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos],
+  );
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [todos],
+  );
   return (
     <TodoTemplate>
-      <TodoInsert/>
-      <TodoList />
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
 }
